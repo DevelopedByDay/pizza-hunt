b653edd6fdac7b0a009d3948447c339c65add88f
BradD <bradleyeday@gmail.com>
2020-09-21 15:04:49 -0500

    commments!

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,3 +1,4 @@
+const moment = require('moment');
 const { Schema, model } = require('mongoose');
 
 
@@ -12,13 +13,32 @@ const PizzaSchema = new Schema({
     },
     createdAt: {
         type: Date,
-        default: Date.now
+        default: Date.now,
+        get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
     },
     size: {
         type: String,
         default: 'Large'
     },
-    toppings: []
+    toppings: [],
+    comments: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Comment'
+        }
+    ]
+    },
+    {
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false
+    }
+);
+
+PizzaSchema.virtual('commentCount').get(function() {
+    return this.comments.length;
 });
 
 const Pizza = model('Pizza', PizzaSchema);
